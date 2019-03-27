1b21d29488eeb45b7dcac1ec1cf8a780e8a6e27c
xk_wang <xk_wang@outlook.com>
2019-03-27 22:25:26 +0800

    add files via uploads

---
+++ 02_end_to_end_machine_learning_project/p2_load_the_data.py
@@ -0,0 +1,14 @@
+#from p1_opt_download_the_data import HOUSING_PATH, os
+import os
+import pandas as pd
+
+HOUSING_PATH = "datasets/housing"
+# Load the data using Pandas
+def load_housing_data(housing_path=HOUSING_PATH):
+    csv_path = os.path.join(housing_path, "housing.csv")
+    return pd.read_csv(csv_path)
+
+housing = load_housing_data()
+
+
+
