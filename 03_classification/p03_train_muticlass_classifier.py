2a71ea3648ba99f13941c7657cf09f27d7a37034
xk_wang <xk_wang@outlook.com>
2019-04-15 22:06:33 +0800

    Add files via upload

--- 03_classification/p03_train_muticlass_classifier.py
+++ 03_classification/p03_train_muticlass_classifier.py
@@ -1,4 +1,17 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
+# The following code solved displaying Chinese issue in plt
+plt.rcParams['font.sans-serif'] = ['SimHei']
+plt.rcParams['axes.unicode_minus'] = False
+
+
+def plot_digit(data):
+    image = data.reshape(28, 28)
+    plt.imshow(image, cmap=plt.cm.gray,
+               interpolation="nearest")
+    plt.axis("off")
+    # plt.show()
 
 
 def load_data():
@@ -29,6 +42,7 @@ X_train, y_train = X_train[shuffle_index], y_train[shuffle_index]
 some_digit = X[36000]
 
 
+# Multi-class Classification
 # OvO strategy
 # Create a multi-class classifier using the OvO strategy,
 # based on a SGDClassifier
@@ -79,29 +93,87 @@ def plot_matrix_and_error_analysis(x_unscaled, y_label):
     from sklearn.linear_model import SGDClassifier
     from sklearn.preprocessing import StandardScaler
     from sklearn.metrics import confusion_matrix
-    import matplotlib.pyplot as plt
     scaler = StandardScaler()
     x_scaled = scaler.fit_transform(x_unscaled.astype(np.float64))
     sgd_clf = SGDClassifier(max_iter=5, tol=np.infty, random_state=42)
     y_train_pred = cross_val_predict(sgd_clf, x_scaled, y_label, cv=3)
     conf_mx = confusion_matrix(y_label, y_train_pred)
-    print("\nMuti-Class混淆矩阵:\n", conf_mx)
+    # print("\nMuti-Class混淆矩阵:\n", conf_mx)
     # plot confusion matrix
     plt.matshow(conf_mx, cmap=plt.cm.gray)
     plt.show()
 
     # error analysis
     row_sums = conf_mx.sum(axis=1, keepdims=True)
-    print("row_sums: ", row_sums)
+    # print("row_sums: ", row_sums)
     norm_conf_mx = conf_mx / row_sums
-    print("\nMuti-Class 正则化-混淆矩阵:\n", norm_conf_mx)
+    # print("\nMuti-Class 正则化-混淆矩阵:\n", norm_conf_mx)
     # fill the diagonal with zeros to keep only the errors,
     # and let’s plot the result
     np.fill_diagonal(norm_conf_mx, 0)
-    print("\nfill 0 正则化-混淆矩阵:\n", norm_conf_mx)
+    # print("\nfill 0 正则化-混淆矩阵:\n", norm_conf_mx)
     plt.matshow(norm_conf_mx, cmap=plt.cm.gray)
     plt.show()
 
 
 plot_matrix_and_error_analysis(X_train, y_train)
+
+
+# Multi-label Classification
+def test_for_multilabel_classification(x, y_label):
+    from sklearn.neighbors import KNeighborsClassifier
+    from sklearn.model_selection import cross_val_predict
+    from sklearn.metrics import f1_score
+    y_train_large = (y_label >= 7)
+    y_train_odd = (y_label % 2 == 1)
+    y_multilabel = np.c_[y_train_large, y_train_odd]
+    knn_clf = KNeighborsClassifier()
+    knn_clf.fit(x, y_multilabel)
+    print(y[36000], "的预测结果是", knn_clf.predict([some_digit]))
+    y_train_knn_pred = cross_val_predict(knn_clf, x, y_label, cv=2, verbose=3)
+    print("\nF1 Score: ", f1_score(y_label, y_train_knn_pred, average="macro"))
+
+
+# the following code may take a very long time
+# comment to run faster
+# test_for_multilabel_classification(X_train, y_train)
+
+
+# Multi-output Classification
+def test_for_multioutput_classification(x_train, x_test):
+    import numpy.random as rnd
+    from sklearn.neighbors import KNeighborsClassifier
+    noise_train = rnd.randint(0, 100, (len(x_train), 784))
+    noise_test = rnd.randint(0, 100, (len(x_test), 784))
+    X_train_mod = x_train + noise_train
+    X_test_mod = x_test + noise_test
+    y_train_mod = x_train
+    y_test_mod = x_test
+    some_index = 5500
+    # plot
+    some_index = 5500
+    plt.subplot(121)
+    plt.title("添加噪音图像")
+    plot_digit(X_test_mod[some_index])
+    plt.subplot(122)
+    plt.title("原始图像")
+    plot_digit(y_test_mod[some_index])
+    plt.show()
+    # build a system that removes noise from images
+    knn_clf = KNeighborsClassifier()
+    knn_clf.fit(X_train_mod, y_train_mod)
+    clean_digit = knn_clf.predict([X_test_mod[some_index]])
+    plt.subplot(121)
+    plt.title("原始图像")
+    plot_digit(y_test_mod[some_index])
+    plt.subplot(122)
+    plt.title("去噪(预测)图像")
+    plot_digit(clean_digit)
+    plt.show()
+    # print("\nraw_digit:", X_test_mod[some_index])
+    # print("\nclean_digit:", clean_digit)
+
+
+test_for_multioutput_classification(X_train, X_test)
+
 
